623622eddf0acf26f8b2bc15d18dca78b16fd2ab
Ionel Badisor <ionel.badisor@digilent.ro>
2019-10-11 10:47:24 +0300

    bt: dummy bt emulator: Test if zuca is ready

--- project-spec/meta-user/recipes-apps/zuca-test-suite/files/dummy-bt.sh
+++ project-spec/meta-user/recipes-apps/zuca-test-suite/files/dummy-bt.sh
@@ -2,7 +2,7 @@
 
 function trap_ctrlc(){
 	echo ctrl + c traped;
-	for job in $(jobs -p); do #stop all background job for this tool
+	for job in $(jobs -p); do #stop all background jobs for this tool
 		echo kill $job && kill "$job"
 	done
 	exit 0;
@@ -14,9 +14,11 @@ zuca_serial=$1
 
 stty -F $zuca_serial 115200 cs8 -cstopb -parenb -echo
 #
-##echo test1 > $zuca_serial;
-#
-while read response; do echo in:$response; done < $zuca_serial &
+echo BT-RUNNING
+
+while read response; do echo RX:$response; done < $zuca_serial &
+
+echo -e -n "running\r\n" > $zuca_serial;
 
 while read command; do echo "$command" > $zuca_serial; done;
 
