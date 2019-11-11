7eac6c368efd33042e95eb78abac5c16d87020c4
Ionel Badisor <ionel.badisor@digilent.ro>
2019-11-11 15:25:46 +0200

    u-boot: Set MAC into fdt without uEnv.txt
    Replace MAC address in device tree by defaunt on boot

--- project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
+++ project-spec/meta-user/recipes-bsp/u-boot/files/platform-top.h
@@ -15,6 +15,31 @@
         "dfu_mmc=run dfu_mmc_info && dfu 0 mmc 0\0" \
         "thor_mmc=run dfu_mmc_info && thordown 0 mmc 0\0"
 
+#define CONFIG_PREBOOT "echo U-BOOT for Zuca TEST; "\
+	"setenv img_addr 0x10000000; "\
+	"setenv img_name image.ub; "\
+	"setenv mac_addr 0x13ffff0; "\
+	"setenv mac_qspi_addr 0x1FFF000; "\
+	"setenv mac_length 0x6; "\
+	"setenv mac_dtb_path /amba/ethernet@ff0b0000; "\
+	"setenv mac_dtb_prop local-mac-address; "\
+	"setenv mac_read_qspi_data \"sf read '${mac_addr}' '${mac_qspi_addr}' '${mac_length}';\"; "\
+	"setenv mac_read_dtb_addr \"fdt get addr mac_dtb_addr '${mac_dtb_path}' '${mac_dtb_prop}';\"; "\
+	"setenv mac_write_dtb \"cp.b '${mac_addr}' '${mac_dtb_addr}' '${mac_length}';\"; "\
+	"setenv mac_load \"run mac_read_qspi_data && run mac_read_dtb_addr && run mac_write_dtb;\"; "\
+	"setenv mac_ch_boot \""\
+		"load mmc 0:1 '${img_addr}' '${img_name}' && "\
+		"sf probe && "\
+		"bootm start '$img_addr' && "\
+		"bootm loados && "\
+		"bootm ramdisk && "\
+		"bootm fdt && "\
+		"bootm prep && "\
+		"run mac_load && "\
+		"bootm go; "\
+	"\"; "\
+	"setenv default_bootcmd run mac_ch_boot"
+
 /*Required for uartless designs */
 #ifndef CONFIG_BAUDRATE
 #define CONFIG_BAUDRATE 115200
